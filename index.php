fe6c160435d5259269ff4697e021d15943c23997
gangrel7 <gangrel7@gmail.com>
2018-02-16 22:40:29 -0200

    Adicionados arquivos index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link href="http://fonts.googleapis.com/css?family=Cabin:400,700" rel="stylesheet" type="text/css" />
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<meta name="author" content="brainstorming7" />
+	<meta name="description" content="Momentos especiais devem ser compartilhados."/>
+	<meta name="keywords" content="casamento, noivos, noiva, noivo, 15 anos, debutante, aniversário"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo('/_css/normalize.css'); ?>"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo('/_css/feliciteme.css'); ?>"/>
+	<title>Felicite Me!</title>
+</head>
+<body>
+	<?php include_once("_include/link_config_script_google.inc"); ?>
+	<section id="construcao">
+		<img src="_css/logo_feliciteme.png" alt="Felicite Me!">
+		<h1>Bem vindo!</h1>
+		<p>Nosso site ainda está em fase de construção, mas você já pode se alegrar com nossos primeiros noivos!</p>
+		<p>Para acessar a página da Aline &amp; Diogo clique no link abaixo:</p>
+		<div id="galeria">
+			<a href="/noivos/a/aline-diogo/index.php" title="Aline &amp; Diogo"><img class="desktop" src="noivos/a/aline-diogo/fotos/link_banner_001.jpg" alt="Aline &amp; Diogo"></a>
+			<a href="/noivos/a/aline-diogo/index.php" title="Aline &amp; Diogo"><img class="mobile" src="noivos/a/aline-diogo/fotos/link_banner_peq_001.jpg" alt="Aline &amp; Diogo"></a>
+		</div>
+	</section>
+</body>
+</html>
